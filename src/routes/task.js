2d8b1e8498320ca74bfe7cf6068b0c31604bb227
Azan <azan.momin@gmail.com>
2025-07-07 01:09:12 +0530

    Added read tasks, read task api

--- src/routes/task.js
+++ src/routes/task.js
@@ -3,13 +3,32 @@ const mongoose = require('mongoose');
 const Task = require('../models/task');
 const router = new express.Router();
 
+// Read all tasks
 router.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({});
-        res.status(200).send(tasks);
+        res.send(tasks);
     } catch (e) {
         console.log('Error: ', e);
         res.status(500).send(e);
+    }
+});
+
+// Read a task
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const task = await Task.findById(_id);
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task);
+    } catch (e) {
+        console.log('Error: ', e);
+        res.status(500).send();
     }
 });
 
